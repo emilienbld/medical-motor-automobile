0e3547101c09d8a978b0e7b7fb1d9ed5f0b3c207
lylyss97x <alyssapaygambar@gmail.com>
2025-06-19 10:33:08 +0200

    [Rvm75seu] : Detection de la localisation grâce au module gps

---
+++ arduino/gps/detection_location.ino
@@ -0,0 +1,38 @@
+#include <SoftwareSerial.h>
+#include <TinyGPS++.h>
+
+static const int RXPin = A2;
+static const int TXPin = A1;
+static const uint32_t GPSBaud = 9600;
+
+TinyGPSPlus gps;
+SoftwareSerial gpsSerial(RXPin, TXPin);
+
+void setup() {
+  Serial.begin(9600);
+  gpsSerial.begin(GPSBaud);
+  Serial.println("Software Serial started at 9600 baud rate");
+}
+
+void loop() {
+  while (gpsSerial.available() > 0){
+    gps.encode(gpsSerial.read());
+    if (gps.location.isUpdated()) {
+      Serial.print("LAT: ");
+      Serial.println(gps.location.lat(), 6);
+      Serial.print("LONG: "); 
+      Serial.println(gps.location.lng(), 6);
+      Serial.print("SPEED (km/h) = "); 
+      Serial.println(gps.speed.kmph()); 
+      Serial.print("ALT (min)= "); 
+      Serial.println(gps.altitude.meters());
+      Serial.print("HDOP = "); 
+      Serial.println(gps.hdop.value() / 100.0); 
+      Serial.print("Satellites = "); 
+      Serial.println(gps.satellites.value()); 
+      Serial.print("Time in UTC: ");
+      Serial.println(String(gps.date.year()) + "/" + String(gps.date.month()) + "/" + String(gps.date.day()) + "," + String(gps.time.hour()) + ":" + String(gps.time.minute()) + ":" + String(gps.time.second()));
+      Serial.println("");
+    }
+  }
+}
